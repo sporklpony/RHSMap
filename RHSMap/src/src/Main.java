d09eda014a89389ce721a5189da8f44ff03a8e6d
Aidan Flaherty <aidan.n.flaherty@gmail.com>
2021-04-09 16:28:27 -0400

    Commit

--- RHSMap/src/src/Main.java
+++ RHSMap/src/src/Main.java
@@ -1,5 +1,36 @@
 package src;
 
-public class Main {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Scanner;
 
+import javax.swing.JPanel;
+
+public class Main extends JPanel implements Runnable {
+	
+	public Main() {
+		setVisible(true);
+		setFocusable(true);
+		new Thread(this).start();
+	}
+	
+	public void paint(Graphics g) {
+		setBackground(new Color(255, 255, 255));
+		g.clearRect(0, 0, getWidth(), getHeight());
+		g.drawString("Hello", 0, 0);
+	}
+	
+	public void run() {
+		long a = System.currentTimeMillis();
+		while(true) {
+			repaint();
+			
+			if(a < 30) try {
+				Thread.sleep(30 - a);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			a = System.currentTimeMillis();
+		}
+	}
 }
--- RHSMap/src/src/Window.java
+++ RHSMap/src/src/Window.java
@@ -1,5 +1,17 @@
 package src;
 
-public class Window {
+import javax.swing.JFrame;
 
+public class Window extends JFrame {
+	public Window() {
+		setVisible(true);
+		setFocusable(true);
+		setSize(500, 500);
+		add(new Main());
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Program Starting, yeet!");
+		new Window();
+	}
 }
