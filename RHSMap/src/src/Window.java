e23a3e1a4fa7c24e32bb36432c5ae3f94df4e64d
Aidan Flaherty <aidan.n.flaherty@gmail.com>
2021-04-30 14:39:23 -0400

    Commit

--- RHSMap/src/src/Window.java
+++ RHSMap/src/src/Window.java
@@ -4,10 +4,9 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	public Window() {
-		setVisible(true);
-		setFocusable(true);
-		setSize(500, 500);
 		add(new Main());
+		setVisible(true);
+		setSize(500, 500);
 	}
 	
 	public static void main(String[] args) {
